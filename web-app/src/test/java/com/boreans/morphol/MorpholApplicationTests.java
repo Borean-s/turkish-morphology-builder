43dff282e7b803a994aa9e68f42533fa103ea258
Borean-s <der.boranden@gmail.com>
2026-07-29 15:28:02 +0300

    Add Spring Boot web-app project (Java 21, Spring Boot 4.1, Thymeleaf)

---
+++ web-app/src/test/java/com/boreans/morphol/MorpholApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boreans.morphol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MorpholApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
